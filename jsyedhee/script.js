702e27e0e5b10abece2a6ecb99d8f70c92e21e5f
DeAlone05 <Alonezone2005@gmail.com>
2025-06-19 13:33:55 +0700

    Update script.js

--- jsyedhee/script.js
+++ jsyedhee/script.js
@@ -76,42 +76,41 @@ function fetchProfile() {
       document.getElementById('username').textContent = 'ไม่สามารถโหลดโปรไฟล์';
       document.getElementById('status').textContent = '';
     });
-})
-
-const playlist = [
-  "https://raw.githubusercontent.com/Dogearth/mp3/main/hee.mp3",
-  "https://raw.githubusercontent.com/Dogearth/mp3/main/Ego.mp3"
-];
-
-let currentIndex = 0;
-let audio;
-
-function enterSite() {
-  document.querySelector('.enter-btn').style.display = 'none';
-  document.getElementById('profile').style.display = 'flex';
-
-  audio = document.getElementById('myAudio');
-  audio.volume = 0.1;
-  audio.loop = false; 
-  playCurrent();
-  audio.addEventListener('ended', playNext);
-
-  document.addEventListener('keydown', (event) => {
-    if (event.code === 'Space') {
-      event.preventDefault();
-      audio.paused ? audio.play() : audio.pause();
-    }
-  });
-}
-
-function playCurrent() {
-  audio.src = playlist[currentIndex];
-  audio.play().catch(err => console.warn('เล่นเพลงไม่ได้:', err));
-}
-
-function playNext() {
-  currentIndex = (currentIndex + 1) % playlist.length;
-  playCurrent();
-}
-fetchProfile(); 
-setInterval(fetchProfile, 1000); 
+          const playlist = [
+          "https://raw.githubusercontent.com/Dogearth/mp3/main/hee.mp3",
+          "https://raw.githubusercontent.com/Dogearth/mp3/main/Ego.mp3"
+        ];
+        
+        let currentIndex = 0;
+        let audio;
+        
+        function enterSite() {
+          document.querySelector('.enter-btn').style.display = 'none';
+          document.getElementById('profile').style.display = 'flex';
+        
+          audio = document.getElementById('myAudio');
+          audio.volume = 0.1;
+          audio.loop = false; 
+          playCurrent();
+          audio.addEventListener('ended', playNext);
+        
+          document.addEventListener('keydown', (event) => {
+            if (event.code === 'Space') {
+              event.preventDefault();
+              audio.paused ? audio.play() : audio.pause();
+            }
+          });
+        }
+        
+        function playCurrent() {
+          audio.src = playlist[currentIndex];
+          audio.play().catch(err => console.warn('เล่นเพลงไม่ได้:', err));
+        }
+        
+        function playNext() {
+          currentIndex = (currentIndex + 1) % playlist.length;
+          playCurrent();
+        }
+          fetchProfile(); 
+          setInterval(fetchProfile, 1000); 
+      }
