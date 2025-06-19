fa8ecf5cc86772e71a858c227813292e7bd2df8b
DeAlone05 <Alonezone2005@gmail.com>
2025-06-19 09:30:30 +0700

    Update script.js

--- jsyedhee/script.js
+++ jsyedhee/script.js
@@ -79,8 +79,7 @@ function fetchProfile() {
 }
 
 const playlist = [
-  "https://raw.githubusercontent.com/Dogearth/mp3/main/hee.mp3",
-  "https://raw.githubusercontent.com/Dogearth/mp3/main/EGO.mp3"
+  "https://raw.githubusercontent.com/Dogearth/mp3/main/hee.mp3"
 ];
 
 let currentIndex = 0;
